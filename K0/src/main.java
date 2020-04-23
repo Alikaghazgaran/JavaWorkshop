2627776c10f715c7ed9a30226a4bed3aaa5b930a
Ali Kaghazgaran <59315074+Alikaghazgaran@users.noreply.github.com>
2020-04-23 21:15:23 +0430

    Add files via upload

---
+++ K0/src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int n=2 , i;
+        for (i=1 ; i<11 ; i++) {
+            System.out.println("2 to the power of " + i + ": " + n);
+            n *= 2;
+        }
+    }
+}
+//چاپ توان های عدد ۲ تا توان ۱۰
